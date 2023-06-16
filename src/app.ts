da8a07991f67f0ca26a54df27b749e5b7f16aea2
Aliesky Perez <aliesky.perez@gmail.com>
2023-06-16 01:51:45 -0400

    implemented cors

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,10 @@ import { json } from "body-parser";
 //APPLICATION ROUTES
 import { articleRoutes } from "./routes";
 import { initMongoConnect } from "./dataBase";
+import cors from 'cors'
 
 const app = express();
+app.use(cors())
 app.use(json());
 
 app.use("/api", articleRoutes);
